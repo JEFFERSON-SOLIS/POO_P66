4fb3b4145e1424087788991d2d10c3dea0ea4b85
JEFFERSON-SOLIS <jsolish@est.ups.edu.ec>
2025-03-24 10:47:19 -0500

    Add files via upload

---
+++ practica 24-03.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 24 10:31:28 2025
+
+@author: lab
+"""
+
+country=[
+    "Brazil",
+    "Russia",
+    "India",
+    "China",
+    "South Africa"
+    ]
+capitals={
+    "Brazil":"Brasilia",
+    "Russia":"Moscow",
+    "India":"New Delhi",
+    "China":"Beijing",
+    "South Africa":"Ciudad del Cabo"
+    }
+print ('country')
+print ('capitals')
+print (capitals["South Africa"][1])
