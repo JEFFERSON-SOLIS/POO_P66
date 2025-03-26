4b9accc47f6feeb1a6de9e5578023ec0fcf7ebfe
JEFFERSON-SOLIS <jsolish@est.ups.edu.ec>
2025-03-26 10:45:47 -0500

    Add files via upload

---
+++ funciones26-03.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+#def message() :
+ #   print("enter next value")
+#print ("we start here")
+#message()
+#print ("the end is here")
+#/////////////////////////////////////////////////////////////////////
+#def mensaje() :
+#    print("Ingrese un valor")
+#mensaje()
+#a= int(input())
+#mensaje()
+#b= int(input())
+#mensaje()
+#c= int(input())
+#print("El valor es:", a+b+c)
+#//////////////////////////////////////////////////////////////////////
+#def saludo (name,apellido):
+#    print("hola,",name, apellido)
+#saludo("juan","dominguez")
+#a=input("Ingrese su nombre :")
+#b=input("ingrese su apellido :")
+#saludo(a,b)
+#////////////////////////////////////////////////////////////////////////
+#def resta(a,b):
+#    print(a-b)
+#resta(2, 3)
+#////////////////////////////////////////////////////////////////////////
+#def multi(a,b):
+#    print(a*b)
+#multi(a=4, b=23)
+#/////////////////////////////////////////////////////////////////////////
+#def resta(a,b):
+#    print(a-b)
+#resta(2,b=3)
+#resta(2,3)
+#///////////////////////////////////////////////////////////////////////
+#def multi(a,b):
+#    print(a*b)
+#multi(a=4, b=23)
+#////////////////////////////////////////////////////////////////////////
+#def multi(a,b):
+#    print(a*b)
+#    return(a*b)
+#multi(a=4, b=23)
+#
+#x=(2300,3)
+#opt= x+23
+#print(opt)
+#///////////////////////////////////////////////////////////////////////////
+#def holatodoelmundo(miLista) :
+#    for name in miLista:
+#        print("hola,", name)
+#holatodoelmundo(["juan","pedro","pablo","carlos"])
+#/////////////////////////////////////////////////////////////////////////////
+#def crearlista(n):
+#    milista=[]
+#    for i in range(n):
+#        milista.append(i)
+#    return milista
+#print (crearlista(19))
+#////////////////////////////////////////////////////////////////////////////    
+#def suma(*a):
+#    print("\n\tTipo de datos del argumento:",
+#         type(a))
+#    sum = 0
+#    for n in a:
+#        sum +=n
+#        #sum=sum+n
+#
+#    print("Suma:",sum)
+#suma(1)
+#suma(5,8)
+#suma(1,6)
+#suma (2,8,10)
+#suma(0,1)
+#suma(1,2)
+#suma(3,5)
+#suma(4,5,6,7)
+#suma(1,2,3,5,6)
+#suma(1,2,3,5,6,7,8,9,10)
+#suma(1,2,3,5,6,7,8,9,10,11,12,13,14)
+#suma(1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17)
+#//////////////////////////////////////////////////////////////////////////////
+#def keyw(**datos):
+#    print("\nTipo de datos del argumento:",
+#          type(datos))
+#
+#    for key, value in datos.items():
+#        print("{} is {}".format(key,value))
+#
+#keyw(Firstname="Juan", 
+#     Lastname="Domínguez", 
+#     Age=42,    
+#     Phone=1234567890)
+#keyw(Firstname="John", 
+#     Lastname="Wood",
+#     Email="johnwood@nomail.com",
+#     Country="Wakanda", 
+#     Age=25, 
+#     Phone=9876543210)
+#keyw(Firstname="Juan", 
+#     Lastname="Domínguez", 
+#     )
+#//////////////////////////////////////////////////////////////////////////////
