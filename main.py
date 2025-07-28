2bbcb36bd4b2e87615e27bb0fb4ee523be87c749
JEFFERSON-SOLIS <jsolish@est.ups.edu.ec>
2025-07-28 09:22:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-#Calculadora Basicas 
-def sumar(a, b):
-    return a + b
+from bot import discord_bot
 
-def restar(a, b):
-    return a - b
-
-def multiplicar(a, b):
-    return a * b
-
-def dividir(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Error: División por cero"
-
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
-operacion = input("Selecciona la operación (sumar, restar, multiplicar, dividir): ")
-
-if operacion == "sumar":
-    print(sumar(num1, num2))
-elif operacion == "restar":
-    print(restar(num1, num2))
-elif operacion == "multiplicar":
-    print(multiplicar(num1, num2))
-elif operacion == "dividir":
-    print(dividir(num1, num2))
-else:
-    print("Operación no válida.")
+if __name__ == "__main__":
+    
+    TOKEN = "token Discord"
+    discord_bot.bot.run(TOKEN)
